cf037768ec42a6bc12e99b1dfcfdec1c516c39e2
TheCats100 <m-g-152@outlook.fr>
2020-06-19 17:01:16 +0200

    package

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const connection = require('./conf')
 
 app.use(express.json());
 app.use(express.urlencoded({
@@ -8,7 +9,56 @@ app.use(express.urlencoded({
 }));
 
 app.get('/', (request, response) => {
-  response.json({message: 'Bienvenue sur Express'} );
+  response.json({ message: 'Bienvenue sur Express' });
+});
+
+app.get('/users', (req, res) => {
+  connection.query('SELECT * from Users', (err, results) => {
+    if (err) {
+      return (
+        res.status(500).json({ message: 'No body was here..' })
+      )
+    }
+    res.json(results);
+  });
+});
+
+app.get('/users/comments', (req, res) => {
+  connection.query('SELECT comment from Users', (err, results) => {
+    if (err) {
+      return (
+        res.status(500).json({ message: 'No body was here..' })
+      )
+    }
+    res.json(results);
+  });
+});
+
+
+app.get('/users/comments/filter/', (req, res) => {
+  const { query } = req
+  if (query.content) {
+    const queryContent = `%${query.content}%`
+    connection.query('SELECT comment from Users WHERE comment LIKE ?', queryContent, (err, results) => {
+      if (err) {
+        return (
+          res.status(500).json({ message: 'No body was here..' })
+        )
+      }
+      res.json(results);
+    });
+  }
+  if (query.date) {
+    const queryDate =`${query.date} 00:00:00`
+    connection.query('SELECT comment from Users WHERE date > ?', queryDate, (err, results) => {
+      if (err) {
+        return (
+          res.status(500).json({ message: 'No body was here..' })
+        )
+      }
+      res.json(results);
+    });
+  }
 });
 
 module.exports = app; 
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const port = process.env.port || 8000;
+const port = process.env.port || 3000;
 
 app.listen(port, (err) => {
   if (err) {
